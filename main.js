f113b7192a9ba2d99ae921fcf6e15751f01dbb3b
Snazzynuhu <harunanuhu17@gmail.com>
2021-11-23 15:18:14 +0100

    completed challenge

---
+++ main.js
@@ -0,0 +1,9 @@
+const sectionOne = document.querySelector(".section-one");
+
+const billBtn = document.querySelector(".billing-button").addEventListener("click", ()=>{
+    sectionOne.classList.toggle("show");
+})
+
+console.log(sectionOne);
+
+// slider
